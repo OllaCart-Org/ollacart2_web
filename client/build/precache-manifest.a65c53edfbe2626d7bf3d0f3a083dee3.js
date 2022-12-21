06d9d895f1a527e046011d92d453c7eb200e2d0d
unknown <aaa0109@mailbox.org>
2022-12-21 02:51:12 +0200

    update quickvie

--- client/build/precache-manifest.a65c53edfbe2626d7bf3d0f3a083dee3.js
+++ client/build/precache-manifest.a65c53edfbe2626d7bf3d0f3a083dee3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8a58406a5725a94925976e1ef27480d",
+    "revision": "823a3bc2bf63b4407a15b11385540b10",
     "url": "/index.html"
   },
   {
-    "revision": "4c08b2f8dc689cc101b1",
-    "url": "/static/css/main.030f19de.chunk.css"
+    "revision": "64929b3f1f2a7f7dc80a",
+    "url": "/static/css/main.800d1e23.chunk.css"
   },
   {
     "revision": "6ff462286a83db3ffa2e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6c0b39b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4c08b2f8dc689cc101b1",
-    "url": "/static/js/main.637a15cf.chunk.js"
+    "revision": "64929b3f1f2a7f7dc80a",
+    "url": "/static/js/main.54991336.chunk.js"
   },
   {
     "revision": "7cf7d97aca095065d8f6",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a54c5941ac1e464293d858b329dbc4b9.js"
+  "/precache-manifest.a65c53edfbe2626d7bf3d0f3a083dee3.js"
 );
 
 self.addEventListener('message', (event) => {
