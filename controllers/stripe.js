81325cc37815cb139b9793c73dd9d2443426c83d
unknown <aaa0109@mailbox.org>
2023-01-29 18:23:03 +0200

    test webhook

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -142,9 +142,9 @@ exports.receiveWebhook = async (req, res) => {
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
+    res.status(400).send(`Webhook Error: ${err.message}`);
     return;
   }
 
