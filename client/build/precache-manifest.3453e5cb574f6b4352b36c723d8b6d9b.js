551d7f712cd8e9497eee6ec2d2c1263d10214e32
unknown <aaa0109@mailbox.org>
2023-02-10 20:45:33 +0200

    back to dev

--- client/build/precache-manifest.3453e5cb574f6b4352b36c723d8b6d9b.js
+++ client/build/precache-manifest.3453e5cb574f6b4352b36c723d8b6d9b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "441579fdccfff8f1457b75c628f78883",
+    "revision": "f9c70d3e9c702e0e208d6c0fd3de464b",
     "url": "/index.html"
   },
   {
-    "revision": "3d6542cd540de17e21ee",
+    "revision": "8b4d4085ac0f0af605d0",
     "url": "/static/css/main.562ae2f8.chunk.css"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3e6ddbe9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3d6542cd540de17e21ee",
-    "url": "/static/js/main.82193444.chunk.js"
+    "revision": "8b4d4085ac0f0af605d0",
+    "url": "/static/js/main.8510ccf6.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/static/js/main.82193444.chunk.js.LICENSE.txt"
+    "url": "/static/js/main.8510ccf6.chunk.js.LICENSE.txt"
   },
   {
     "revision": "7cf7d97aca095065d8f6",
